17da3a297179ad4d93589073d3ab4b0719941e59
OhMedrano <Omedrano564@gmail.com>
2016-07-10 00:19:07 -0400

    somewhat finished the center's page

--- app/scripts/controllers/centers.js
+++ app/scripts/controllers/centers.js
@@ -9,9 +9,57 @@
  */
 angular.module('jaredv2App')
   .controller('CentersCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.dataCenter = [{
+    				'name':'725 S. Wells, Chicago',
+    				'space':6000,
+    				'fload':250,
+    				'powDen':10,
+    				'infra':[
+    					'5',
+    					'UPS - 750kVA Chkloride UPS N+1, 15 minute battery capacity',
+    					'1.5MW + 1MW Generators',
+    					'230+ ton Smardt Chillers',
+    					'4x 70 ton CRAC units'
+
+
+    				],
+    				'networks':[
+    				'SteadFast Networks',
+    				'AboveNet',
+    				'AT&T',
+    				'Verzion',
+    				'Qwest',
+    				'Level(3)',
+    				'Cogent','Sidera','T-Systems','XO'
+    				]
+    				
+    			},
+    			{
+    				'name':'350 E. Cermak, Chicago',
+    				'space':16000,
+    				'fload':250,
+    				'powDen':12,
+    				'infra':[
+    					'100+MW and 3 substations',
+    					'2N redundant power distribution',
+    					'UPS - 2N 750kVA Libert UPS, 15 minute battery capacity',
+    					'2MW + 1MW Generators',
+    					'Building-provided redundant chilled water',
+    					'16x 20 ton CRAC units'
+
+
+    				],
+    				'networks':[
+    				'SteadFast Networks',
+    				'AboveNet',
+    				'AT&T',
+    				'Verzion',
+    				'Qwest',
+    				'Level(3)',
+    				'BT',
+    				'CME','ICE','Cogent','Sidera','T-Systems','XO'
+    				]
+    				
+    			}
     ];
   });
